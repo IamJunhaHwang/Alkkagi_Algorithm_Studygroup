7dd441d5e82ab95caf4b3a86024d045c2de9d3cd
Hong-JInSuk <86042721+Hong-JInSuk@users.noreply.github.com>
2022-05-23 15:29:32 +0900

    Update 백준2022, 사다리.cpp

--- Hong_JinSuk/백준2022, 사다리.cpp
+++ Hong_JinSuk/백준2022, 사다리.cpp
@@ -12,12 +12,13 @@ int main() {
 	float Left = 0;
 	float Right = x;
 
-	for (int i = 0; i < 1000; i++) {
-		float k = (Left + Right) / 2;
+	for (int i = 0; i < 1000; i++) { // 10^-3이 1000번 하라는 뜻??
+		
+		float k = (Left + Right) / 2; // 이진방법으로 오차 좁힐예정
 		float X = sqrt(x * x - k * k);
 		float Y = sqrt(y * y - k * k);
 
-		if (-(X * c / Y) + X > c) Left = k;
+		if (-(X * c / Y) + X > c) Left = k; // f(두 교점의 x 좌표) = -(Xc)/Y+x = c , f(x)는 직선 x에 대한 방정식
 		else Right = k;
 	}
 
