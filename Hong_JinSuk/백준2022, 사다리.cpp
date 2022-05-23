922e1b6048fbde83c70dc38a807df4b86bea103b
Hong-JInSuk <86042721+Hong-JInSuk@users.noreply.github.com>
2022-05-23 15:26:19 +0900

    9주차, 백준2022_사다리

---
+++ Hong_JinSuk/백준2022, 사다리.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+
+	float x, y, c;
+	cin >> x >> y >> c;
+
+	float Left = 0;
+	float Right = x;
+
+	for (int i = 0; i < 1000; i++) {
+		float k = (Left + Right) / 2;
+		float X = sqrt(x * x - k * k);
+		float Y = sqrt(y * y - k * k);
+
+		if (-(X * c / Y) + X > c) Left = k;
+		else Right = k;
+	}
+
+	printf("%.3f", Left);
+	return 0;
+}
