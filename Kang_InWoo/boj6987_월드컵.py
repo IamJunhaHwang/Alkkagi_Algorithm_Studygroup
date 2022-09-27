cba04cf940da3ea25063659fa5d4bcaf826fa329
kiwi_j500 <mingureion@gmail.com>
2022-09-27 23:05:15 +0900

    qksfPckwdmausaksdnjswna

--- Kang_InWoo/boj6987_월드컵.py
+++ Kang_InWoo/boj6987_월드컵.py
@@ -1,51 +1,81 @@
 from sys import stdin
 
-
 arrs = [list(map(int,stdin.readline().split())) for i in range(4)]
 
-for arr in arrs:
-    if arrs.index(arr)!=0:  print(' ', end=' ')
-    
-    #1)총승리수(인덱스를 3으로 나눴을때 첫번째 원소의 합) - 총패배수가 0이 아닐때
-    if (sum(arr[0::3]) - sum(arr[2::3])) != 0:   
-        print(0,end='[1]')
-        continue
-    
-    #2) 6이 입력될 수 있다고 되있지만 팀당 5경기만 하기 때문에 불가능하다
-    if 6 in arr:    
-        print(0,end='[2]')
-        continue
-    
-    #3) 무승부 개수를 조합해서 0이 나올 수 있어야함 
-    drw = 0
-    for i in range(0,18,3):   # 변수 drw가 0이하 일때 + 아니면 -, 마지막에 0이되면 통과
-        if arr[i+1]==0:    continue
-        if drw<=0:  drw += arr[i+1]
-        else:       drw -= arr[i+1]
-    if drw!=0: 
-        print(0,end='[3]')
-        continue
+"""
+무승부 개수 리스트로 가장 큰원소(팀)의 경기를 차례로 지워서 최종적으로 [0] 형태가 나올수 있어야함.
+ex) 
+[5,4,1,0,0,0] -> 5무승부인 팀과 무승부가 0인팀이 같이 있을 수 없으므로 처음부터 불가능한 형태
+[4,4,2,1,1,0] -> [3,1,0,0,0] -> 3과 함께 지울수 있는 경기가 다른팀에 없으므로 불가능한 형태
+"""
+def drw(lis):
+    for i in range(5):
+        p = lis.pop(0)
+        if p>len(lis):  return False    #런타임 오류방지, 오답 입력되면 리스트범위 넘어갈수있음
+        for j in range(p):  lis[j]-=1
+        lis.sort(reverse=True)
+        if -1 in lis:   return False
+    if lis[0]!=0: return False
+    return True
 
-    #4) 각 팀당 승 무 패 숫자의 합은 5여야 한다
+def findidx(lis,n): #리스트에서 n번째로 큰수의 인덱스 반환 + 중복 방지
+    lisc = lis.copy()
+    idx = 0
+    for i in range(n):
+        bigst = max(lisc)
+        idx = lisc.index(bigst)        
+        lisc[idx]= -1
+    return idx
+
+"""
+승리수리스트, 패배수리스트를 받아
+승리수와 타팀의 패배수를 하나씩 깜
+최종적으로 패배 리스트는 [0,0,0,0,0,0]혹은 1만남아있는 형태가 되야함
+"""
+def winlose(w,l):
+    for i in range(6):
+        er =[]  # 패배 리스트에서 지울 인덱스
+        mw = findidx(w,i+1) # w에서 i번쨰로 큰수
+        if w[mw] == 0: break
+        tmp = l.copy()      # l에서 최대값을 차례대로 찾기위해 복사한 임시 리스트
+        tmp[mw]=-1          # 현재 승리를 지우는 같은팀에서 최대값이 안나오도록
+        for j in range(w[mw]):
+            eridx = findidx(tmp,j+1)
+            er.append(eridx)
+        for j in er: l[j]-=1
+        
+        if min(l)<0:    return False
+        if min(l)==0 and max(l)==1: return True
+    if l.count(0)!=6: return False
+    return True
+
+for arr in arrs:            
+    #1) 총 승리 수 = 총 패배 수
+    if (sum(arr[0::3]) - sum(arr[2::3])) != 0:   
+        print(0,end=' ')
+        continue
+    
+    #2) 각 팀당 승 무 패 개수 합은 5
     try:
         for i in range(0,18,3):  
             if sum(arr[i:i+3:1]) != 5:
-                print("sum",i,"~",i+3,sum(arr[i:i+3:1]))
-                print(0,end='[4]')
+                print(0,end=' ')
                 raise Exception()
-    
-    except:         #예외처리로 틀렸을 때 바로 바깥반복문에서 continue되도록
+    except:   #예외처리로 틀렸을 때 바깥반복문에서 continue되도록
         continue
     
-    else:
-        print(1,end='')    
+    #3) 무승부 수 맞추기
+    if drw(sorted(arr[1::3],reverse=True))==False:
+        print(0,end=' ')
+        continue
+    
+    #4) 승-패 수 맞추기
+    if winlose(arr[0::3],arr[2::3])==False:
+        print(0,end=' ')
+        continue
         
-    
-    # 무승부, 승-패 수 맞추는 조건필요
-    # 반례  1 4 0 0 4 1 2 2 1 1 2 2 4 0 1 1 0 4
-    #       5 0 0 2 3 0 2 2 1 1 1 3 1 1 3 0 1 4
-    
-    
-    
-        
+    print(1,end=' ')    
+
+# 틀렸습니다틀렸습니다틀렸습니다틀렸습니다틀렸습니다틀렸습니다틀렸습니다
+
     
