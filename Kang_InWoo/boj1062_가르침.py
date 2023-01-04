d73af414e6ccc25763a4628046c30c2405a093f5
kiw331 <100690081+kiw331@users.noreply.github.com>
2023-01-05 06:02:19 +0900

    solved boj1062_가르침

--- Kang_InWoo/boj1062_가르침.py
+++ Kang_InWoo/boj1062_가르침.py
@@ -1,38 +1,24 @@
-import sys
-from itertools import permutations
-n, k = map(int,sys.stdin.readline().split()) 
-words = [set(sys.stdin.readline().rstrip()[4:-4]) for _ in range(n)]
+from sys import stdin
+from itertools import combinations
+n, k = map(int,stdin.readline().split()) 
+words = [set(stdin.readline().rstrip()[4:-4])-{'a','n','t','i','c'} for _ in range(n)] #기본 글자 5개를 제외한 단어들
 
-if k<5: 
-    print(0)
-    sys.exit()
+res =0
+if k>=5: # 배우는 글자가 5개 미만이면 어떤 단어도 읽지 못함
+    all = set() # (기본글자 제외한) 모든 글자 집합 
+    for w in words:
+        all = all.union(w)
+    if len(all)<=k-5: # 모든 글자보다 배우는 글자가 더 많으면 모든 단어 읽을 수 있음
+        res = n
 
-def isreadable(word,readable):
-    if word<=readable|default: #부분집합(모든 글자를 읽을 수있는지) 판별
-        return True
-    return False
-
-default = {'a','n','t','i','c'}    
-all = set() # 모든 글자 집합 
-for w in words:
-    all = all.union(w)
-
-all = all-default
-if len(all)<=k-5: # 모든 글자보다 배우는 글자가 더 많으면 모든 단어 읽을 수 있음
-    print(n)
-    sys.exit()
-
-readable=[set(x) for x in permutations(all, k-5)] # 읽을 수 있는 글자 조합 
-
-print('all',all)
-print('readable: ',readable)
-
-# 읽을 수 있는 글자 조합 별 읽을 수 있는 단어 개수를 구함
-res = [0]*len(readable) + [0]
-for i in range(len(readable)):
-    for word in words:
-        if isreadable(word,readable[i]):    
-            res[i] +=1
-
-print(max(res))
-    
+    else: # 읽을 수 있는 글자 조합 별 읽을 수 있는 단어 개수를 구함
+        res = 0
+        for com in combinations(all, k-5):
+            case = 0
+            com = set(com)
+            for word in words:
+                if word<=com:    #부분집합인지(모든 글자를 읽을 수있는지) 판별
+                    case +=1
+            if case>res:
+                res = case
+print(res)
