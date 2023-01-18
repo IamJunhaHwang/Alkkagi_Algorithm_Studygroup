fa2f3fed5283bfd0a1f196971822b3544d07f414
stoneTiger0912 <seokbeom0912@gmail.com>
2023-01-18 19:45:34 +0900

    1753 주석

--- Lee_SeokBeom/1753.py
+++ Lee_SeokBeom/1753.py
@@ -19,18 +19,27 @@ for _ in range(m):
 def dijkstra(start):
     q = []
 
+    #가장 우선순위가 높은 데이터 먼저삭제, 현재 가장 가까운 노드를 저장하기 위한 목적
     heapq.heappush(q, (0, start))
     distance[start] = 0
 
+    
+    #큐가 비어있지 않으면
     while q:
+        #가장짧은 거리가 짧은 노드 출력
         dist, now = heapq.heappop(q)
         
+        #기존의 경로의 길이가 적다면 넘어감
         if distance[now] < dist:
             continue
         
+        #기존의 길이보다 크거나 같은경우
+
         for i in graph[now]:
+            #i[1]은 크기
             cost = dist + i[1]
             
+            #현재 노드를 겨처서 이동하는 거리가 짧은 경우 
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
